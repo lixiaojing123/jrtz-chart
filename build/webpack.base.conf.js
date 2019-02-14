9a027b2705b35f5ef01df3856d1eff649391bb54
jingjing <1022928829@qq.com>
2019-02-14 18:48:08 +0800

    #初始化

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,14 +8,23 @@ function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-
+const createLintingRule = () => ({
+  test: /\.(js|vue)$/,
+  loader: 'eslint-loader',
+  enforce: 'pre',
+  include: [resolve('src'), resolve('test')],
+  options: {
+    formatter: require('eslint-friendly-formatter'),
+    emitWarning: !config.dev.showEslintErrorsInOverlay
+  }
+})
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
     app: process.env.NODE_ENV === 'production'
-      ? './src/components/index.js' // 生产模式下导入文件
-      : './src/main.js' // 开发模式下导入文件
+      ? './src/components/index.js'
+      : './src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
@@ -28,11 +37,12 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
     rules: [
+      ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
         test: /\.vue$/,
         loader: 'vue-loader',
